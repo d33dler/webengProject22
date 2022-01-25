903c48193c7480271aa9e7c44797d35a69a83adb
d33dler <r.rebeja@student.rug.nl>
2022-01-26 00:40:21 +0100

    Small static build patch

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://0.0.0.0:6868/nrp/",
+    baseURL: "http://localhost:6868/nrp/",
     headers: {
         "Accept" : "application/json" ,
         "Content-type": "application/json",
         'Access-Control-Allow-Origin': process.env.REACT_APP_API_BASE_URL
     },
-    mode: "cors"
 })
